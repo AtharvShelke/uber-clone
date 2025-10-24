2105326fb3b0a4213a89e857d4e4752f65709879
AtharvShelke <shelkeatharv964@gmail.com>
2025-10-24 10:17:53 +0530

    find-ride page

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,19 +1,51 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocationStore } from '@/store'
+import RideLayout from '@/components/RideLayout';
+import LocationIQTextInput from '@/components/LocationIQTextInput';
+import { icons } from '@/constants';
+import CustomButton from '@/components/CustomButton';
+import { router } from 'expo-router';
 
 const FindRide = () => {
-    const { 
-        userAddress, 
-        destinationAddress, 
+    const {
+        userAddress,
+        destinationAddress,
         setDestinationLocation,
         setUserLocation
     } = useLocationStore();
     return (
-        <View>
-            <Text>{userAddress}</Text>
-            <Text>{destinationAddress}</Text>
-        </View>
+        <RideLayout title='Ride' snapPoints={['85%']}>
+            <View className='my-3'>
+                <Text className='text-lg font-JakartaSemiBold mb-3'>
+                    From:
+                </Text>
+                <LocationIQTextInput
+                    icon={icons.target}
+                    initialLocation={userAddress!}
+                    containerStyle='bg-neutral-100'
+                    textInputBackgroundColor='#f5f5f5'
+                    handlePress={(location) => setUserLocation(location)}
+                />
+            </View>
+            <View className='my-3'>
+                <Text className='text-lg font-JakartaSemiBold mb-3'>
+                    To:
+                </Text>
+                <LocationIQTextInput
+                    icon={icons.map}
+                    initialLocation={destinationAddress!}
+                    containerStyle='bg-neutral-100'
+                    textInputBackgroundColor='#f5f5f5'
+                    handlePress={(location) => setDestinationLocation(location)}
+                />
+            </View>
+            <CustomButton
+                title='Find now'
+                onPress={() => router.push('/(root)/confirm-ride')}
+                className='mt-5'
+            />
+        </RideLayout>
     )
 }
 
